8845fbffd8e6fbf2990b343bd9ca1db95169bc9a
hrloca <hr.loca@gmail.com>
2021-09-25 14:15:31 +0900

    update

--- src/animations.ts
+++ src/animations.ts
@@ -1,5 +1,7 @@
 import { Animated, Easing } from 'react-native'
 
+const frame = (f: number) => 15 * f
+
 export type Frame = {
   coordinate: [number, number]
   duration: number
@@ -27,60 +29,60 @@ export const animations: Animation[] = [
     name: '歩き(前)',
     loop: true,
     frames: [
-      { coordinate: [0, 0], duration: 200 },
-      { coordinate: [1, 0], duration: 200 },
-      { coordinate: [2, 0], duration: 200 },
-      { coordinate: [1, 0], duration: 200 },
+      { coordinate: [0, 0], duration: frame(12) },
+      { coordinate: [1, 0], duration: frame(12) },
+      { coordinate: [2, 0], duration: frame(12) },
+      { coordinate: [1, 0], duration: frame(12) },
     ],
   },
   {
     name: '歩き(後)',
     loop: true,
     frames: [
-      { coordinate: [1, 1], duration: 200 },
-      { coordinate: [2, 1], duration: 200 },
-      { coordinate: [3, 1], duration: 200 },
-      { coordinate: [2, 1], duration: 200 },
+      { coordinate: [1, 1], duration: frame(12) },
+      { coordinate: [2, 1], duration: frame(12) },
+      { coordinate: [3, 1], duration: frame(12) },
+      { coordinate: [2, 1], duration: frame(12) },
     ],
   },
   {
     name: '歩き(左)',
     loop: true,
     frames: [
-      { coordinate: [3, 0], duration: 200 },
-      { coordinate: [4, 0], duration: 200 },
-      { coordinate: [0, 1], duration: 200 },
-      { coordinate: [4, 0], duration: 200 },
+      { coordinate: [3, 0], duration: frame(12) },
+      { coordinate: [4, 0], duration: frame(12) },
+      { coordinate: [0, 1], duration: frame(12) },
+      { coordinate: [4, 0], duration: frame(12) },
     ],
   },
   {
     name: '歩き(右)',
     loop: true,
     frames: [
-      { coordinate: [3, 0], duration: 200, reverse: true },
-      { coordinate: [4, 0], duration: 200, reverse: true },
-      { coordinate: [0, 1], duration: 200, reverse: true },
-      { coordinate: [4, 0], duration: 200, reverse: true },
+      { coordinate: [3, 0], duration: frame(12), reverse: true },
+      { coordinate: [4, 0], duration: frame(12), reverse: true },
+      { coordinate: [0, 1], duration: frame(12), reverse: true },
+      { coordinate: [4, 0], duration: frame(12), reverse: true },
     ],
   },
   {
     name: '走り(前)',
     loop: true,
     frames: [
-      { coordinate: [0, 0], duration: 100 },
-      { coordinate: [1, 0], duration: 100 },
-      { coordinate: [2, 0], duration: 100 },
-      { coordinate: [1, 0], duration: 100 },
+      { coordinate: [0, 0], duration: frame(6) },
+      { coordinate: [1, 0], duration: frame(6) },
+      { coordinate: [2, 0], duration: frame(6) },
+      { coordinate: [1, 0], duration: frame(6) },
     ],
   },
   {
     name: '走り(後)',
     loop: true,
     frames: [
-      { coordinate: [1, 1], duration: 100 },
-      { coordinate: [2, 1], duration: 100 },
-      { coordinate: [3, 1], duration: 100 },
-      { coordinate: [2, 1], duration: 100 },
+      { coordinate: [1, 1], duration: frame(6) },
+      { coordinate: [2, 1], duration: frame(6) },
+      { coordinate: [3, 1], duration: frame(6) },
+      { coordinate: [2, 1], duration: frame(6) },
     ],
   },
   {
@@ -88,75 +90,75 @@ export const animations: Animation[] = [
     loop: true,
     reverse: false,
     frames: [
-      { coordinate: [0, 3], duration: 100 },
-      { coordinate: [1, 3], duration: 100 },
-      { coordinate: [2, 3], duration: 100 },
-      { coordinate: [1, 3], duration: 100 },
+      { coordinate: [0, 3], duration: frame(6) },
+      { coordinate: [1, 3], duration: frame(6) },
+      { coordinate: [2, 3], duration: frame(6) },
+      { coordinate: [1, 3], duration: frame(6) },
     ],
   },
   {
     name: '走り(右)',
     loop: true,
     frames: [
-      { coordinate: [0, 3], duration: 100, reverse: true },
-      { coordinate: [1, 3], duration: 100, reverse: true },
-      { coordinate: [2, 3], duration: 100, reverse: true },
-      { coordinate: [1, 3], duration: 100, reverse: true },
+      { coordinate: [0, 3], duration: frame(6), reverse: true },
+      { coordinate: [1, 3], duration: frame(6), reverse: true },
+      { coordinate: [2, 3], duration: frame(6), reverse: true },
+      { coordinate: [1, 3], duration: frame(6), reverse: true },
     ],
   },
   {
     name: '行動決定(技)',
     loop: false,
     frames: [
-      { coordinate: [4, 0], duration: 50 },
-      { coordinate: [3, 1], duration: 50 },
-      { coordinate: [3, 0], duration: 50, reverse: true },
-      { coordinate: [2, 0], duration: 50 },
-      { coordinate: [4, 2], duration: 300 },
+      { coordinate: [4, 0], duration: frame(3) },
+      { coordinate: [3, 1], duration: frame(3) },
+      { coordinate: [3, 0], duration: frame(3), reverse: true },
+      { coordinate: [2, 0], duration: frame(3) },
+      { coordinate: [4, 2], duration: frame(18) },
     ],
   },
   {
     name: '行動決定(術)',
     loop: false,
     frames: [
-      { coordinate: [0, 1], duration: 100 },
-      { coordinate: [2, 2], duration: 300 },
+      { coordinate: [0, 1], duration: frame(6) },
+      { coordinate: [2, 2], duration: frame(18) },
     ],
   },
   {
     name: '術発動',
     loop: false,
     frames: [
-      { coordinate: [1, 2], duration: 150 },
-      { coordinate: [2, 2], duration: 150 },
-      { coordinate: [1, 2], duration: 150 },
-      { coordinate: [2, 2], duration: 150 },
-      { coordinate: [1, 2], duration: 150 },
-      { coordinate: [2, 2], duration: 150 },
-      { coordinate: [3, 2], duration: 500 },
-      { coordinate: [4, 0], duration: 200 },
+      { coordinate: [1, 2], duration: frame(8) },
+      { coordinate: [2, 2], duration: frame(8) },
+      { coordinate: [1, 2], duration: frame(8) },
+      { coordinate: [2, 2], duration: frame(8) },
+      { coordinate: [1, 2], duration: frame(8) },
+      { coordinate: [2, 2], duration: frame(8) },
+      { coordinate: [3, 2], duration: frame(20) },
+      { coordinate: [4, 0], duration: frame(12) },
     ],
   },
   {
     name: '成長',
     loop: false,
     frames: [
-      { coordinate: [0, 2], duration: 50 },
-      { coordinate: [2, 0], duration: 50 },
-      { coordinate: [0, 1], duration: 50, reverse: true },
-      { coordinate: [1, 1], duration: 50 },
-      { coordinate: [0, 1], duration: 50 },
-      { coordinate: [4, 2], duration: 300 },
+      { coordinate: [0, 2], duration: frame(3) },
+      { coordinate: [2, 0], duration: frame(3) },
+      { coordinate: [0, 1], duration: frame(3), reverse: true },
+      { coordinate: [1, 1], duration: frame(3) },
+      { coordinate: [0, 1], duration: frame(3) },
+      { coordinate: [4, 2], duration: frame(18) },
     ],
   },
   {
     name: 'ダメージ',
     loop: false,
     transition: [
-      { value: { x: 10, y: 0 }, duration: 100, easing: Easing.in(Easing.sin) },
-      { value: { x: 0, y: 0 }, duration: 100, easing: Easing.out(Easing.sin) },
+      { value: { x: 10, y: 0 }, duration: frame(6), easing: Easing.in(Easing.sin) },
+      { value: { x: 0, y: 0 }, duration: frame(6), easing: Easing.out(Easing.sin) },
     ],
-    frames: [{ coordinate: [3, 3], duration: 500 }],
+    frames: [{ coordinate: [3, 3], duration: frame(24) }],
   },
   {
     name: '勝利',
@@ -164,23 +166,118 @@ export const animations: Animation[] = [
     transition: [
       {
         value: { x: 0, y: -100 },
-        delay: 150,
-        duration: 210,
+        delay: frame(14),
+        duration: frame(14),
         easing: Easing.out(Easing.cubic),
       },
-      { value: { x: 0, y: 0 }, duration: 210, easing: Easing.in(Easing.cubic) },
+      { value: { x: 0, y: 0 }, duration: frame(14), easing: Easing.in(Easing.cubic) },
     ],
     frames: [
-      { coordinate: [4, 0], duration: 60 },
-      { coordinate: [4, 3], duration: 90 },
-      { coordinate: [3, 1], duration: 60 }, // jump
-      { coordinate: [3, 0], duration: 60, reverse: true },
-      { coordinate: [2, 0], duration: 60 },
-      { coordinate: [3, 0], duration: 60 }, // rev
-      { coordinate: [3, 1], duration: 60 },
-      { coordinate: [3, 0], duration: 60, reverse: true },
-      { coordinate: [2, 0], duration: 60 },
-      { coordinate: [4, 1], duration: 60 }, // done
+      { coordinate: [4, 0], duration: frame(4) },
+      { coordinate: [4, 3], duration: frame(6) },
+      { coordinate: [3, 1], duration: frame(4) }, // jump
+      { coordinate: [3, 0], duration: frame(4), reverse: true },
+      { coordinate: [2, 0], duration: frame(4) },
+      { coordinate: [3, 0], duration: frame(4) }, // rev
+      { coordinate: [3, 1], duration: frame(4) },
+      { coordinate: [3, 0], duration: frame(4), reverse: true },
+      { coordinate: [2, 0], duration: frame(4) },
+      { coordinate: [4, 1], duration: frame(4) }, // done
+    ],
+  },
+  {
+    name: 'おまけ(円舞剣)',
+    loop: false,
+    transition: [
+      {
+        value: { x: -25, y: -10 },
+        delay: 400,
+        duration: frame(3),
+        easing: Easing.out(Easing.sin),
+      },
+      {
+        value: { x: -50, y: 0 },
+        duration: frame(3),
+        easing: Easing.in(Easing.sin),
+      },
+      {
+        value: { x: -10, y: 0 },
+        delay: frame(9),
+        duration: frame(9),
+        easing: Easing.out(Easing.sin),
+      },
+      {
+        value: { x: -100, y: 0 },
+        duration: frame(12),
+        easing: Easing.out(Easing.sin),
+      },
+      {
+        value: { x: -50, y: -100 },
+        delay: frame(6),
+        duration: frame(6),
+        easing: Easing.out(Easing.sin),
+      },
+      {
+        value: { x: 0, y: 0 },
+        duration: frame(6),
+        easing: Easing.in(Easing.sin),
+      },
+      {
+        value: { x: -50, y: -100 },
+        delay: frame(3),
+        duration: frame(6),
+        easing: Easing.out(Easing.sin),
+      },
+      {
+        value: { x: -100, y: 0 },
+        duration: frame(6),
+        easing: Easing.in(Easing.sin),
+      },
+      {
+        value: { x: -120, y: 0 },
+        duration: frame(4),
+        easing: Easing.in(Easing.sin),
+      },
+      {
+        value: { x: -60, y: -150 },
+        delay: frame(6),
+        duration: frame(9),
+        easing: Easing.in(Easing.sin),
+      },
+      {
+        value: { x: -0, y: 0 },
+        duration: frame(9),
+        easing: Easing.in(Easing.sin),
+      },
+      {
+        value: { x: 10, y: 0 },
+        duration: frame(6),
+        easing: Easing.in(Easing.sin),
+      },
+    ],
+    frames: [
+      { coordinate: [4, 1], duration: 400 },
+      { coordinate: [2, 3], duration: frame(6) }, // Jump
+      { coordinate: [0, 2], duration: frame(3) }, // ReadySword, Landing
+      { coordinate: [4, 1], duration: frame(6) }, // HoldSword
+      { coordinate: [0, 2], duration: frame(9) },
+      { coordinate: [0, 3], duration: frame(6) },
+      { coordinate: [1, 3], duration: frame(6) },
+      { coordinate: [4, 3], duration: frame(6) },
+      { coordinate: [0, 2], duration: frame(15) },
+      { coordinate: [1, 3], duration: frame(6) },
+      { coordinate: [0, 3], duration: frame(6) },
+      { coordinate: [1, 3], duration: frame(6) },
+      { coordinate: [4, 3], duration: frame(6) },
+
+      { coordinate: [2, 3], duration: frame(5) },
+      { coordinate: [1, 1], duration: frame(5) },
+      { coordinate: [1, 3], duration: frame(5), reverse: true },
+      { coordinate: [0, 1], duration: frame(3) },
+
+      { coordinate: [4, 3], duration: frame(9) },
+      { coordinate: [3, 0], duration: frame(9) },
+      { coordinate: [4, 0], duration: 400 },
     ],
   },
 ]
